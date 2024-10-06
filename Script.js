8151e95a89f8b28ad896acb585f1aa9874c6a120
gbohunmi <ayomidefriend5@gmail.com>
2024-10-06 20:38:46 +0100

    commit

--- Script.js
+++ Script.js
@@ -6,3 +6,22 @@ function hide(){
 document.getElementById("phone").style.right = "-100%"
 
 }
+var swiper = new Swiper(".mySwiper", {
+    effect: "cube",
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 10,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+ 
